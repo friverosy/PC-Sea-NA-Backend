743d85c6a9fcc1cbf2edaa8522e098a5397e4097
Jonathan Antognini <jantogni@csrg.cl>
2017-03-29 02:02:20 -0300

    Adding post for itinerary

--- script/cron.py
+++ script/cron.py
@@ -5,20 +5,22 @@ from StringIO import StringIO
 import getopt
 
 TOKEN = '860a2e8f6b125e4c7b9bc83709a0ac1ddac9d40f'
+TOKEN_NAV = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OGRiM2I3NGI0ODRjOTIyOTVmMTE3MWUiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE0OTA3NjI2MTl9.pHVwA2u0iaVhjJ_ljU0NtFR_y0EGCwKXsLgIKSUcCK8'
+NAV_API_URL = 'http://localhost:9000/api/'
 
 def getItineraries(date):
-    print 'Getting itineraries from:', date
+    #print 'Getting itineraries from:', date
     
     url_imaginex_itineraries = 'http://ticket.bsale.cl/control_api/itineraries?date=' + date
     response = requests.get(url_imaginex_itineraries, headers={'token': TOKEN})
     
     itineraries = json.loads(response.text)
-    print itineraries
+    #print itineraries
 
     return itineraries
 
 def getPorts(itinerary_id):
-    print 'Itinerary:', itinerary_id
+    #print 'Itinerary:', itinerary_id
     
     url_imaginex_ports = 'http://ticket.bsale.cl/control_api/itinerary_ports?itinerary=' + str(itinerary_id)
     response = requests.get(url_imaginex_ports, headers={'token': TOKEN})
@@ -28,28 +30,44 @@ def getPorts(itinerary_id):
     return ports
 
 def getInitialManifest(itinerary_id, port_id):
-    print 'Itinerary:', itinerary_id, 'Port:', port_id
+    #print 'Itinerary:', itinerary_id, 'Port:', port_id
     
     url_imaginex_manifest = 'http://ticket.bsale.cl/control_api/itinerary_manifest?itinerary=' + str(itinerary_id) + '&port=' + str(port_id)
     response = requests.get(url_imaginex_manifest, headers={'token': TOKEN})
     
     manifest = json.loads(response.text)
     
-    print manifest
+    #print manifest
 
     return manifest
 
 def getUpdatedManifest(itinerary_id, port_id, update_time):
-    print 'Itinerary:', itinerary_id, 'Port:', port_id
+    #print 'Itinerary:', itinerary_id, 'Port:', port_id
     
     url_imaginex_manifest = 'http://ticket.bsale.cl/control_api/itinerary_manifest?itinerary=' + str(itinerary_id) + '&port=' + str(port_id) + '&date=' + update_time
     response = requests.get(url_imaginex_manifest, headers={'token': TOKEN})
     
     manifest = json.loads(response.text)
     
-    print manifest
+    #print manifest
 
     return manifest
+
+def postItinerary(itinerary):
+    print itinerary
+
+    url_nav_itinerary = NAV_API_URL = 'itineraries/'
+    response = request.post(url_nav_itinerary, data={'refId':itinerary['id_itinerario'], 'depart':itinerary['zarpe'], 'name':itinerary['nombre_ruta']}, headers={'Authorization':'Baerer ' + TOKEN_NAV}
+
+def postPort(port):
+    print port
+    #url_nav_port = NAV_API_URL = 'seaports/'
+    #response = request.post(url_nav_port, data={}, headers={'Authorization':'Baerer ' + TOKEN_NAV}
+
+def postManifest(manifest):
+    print manifest
+    #url_nav_manifest = NAV_API_URL = 'manifests/'
+    #response = request.post(url_nav_manifest, data={}, headers={'Authorization':'Baerer ' + TOKEN_NAV}
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'd:u:h', ['date=', 'update=', 'help='])
@@ -67,16 +85,24 @@ for opt, arg in opts:
 
         itineraries = getItineraries(date)
         
-        print ''
-        print 'Getting Ports Associated to each itinerary'
+        #print ''
+        #print 'Getting Ports Associated to each itinerary'
         for keyword in itineraries:
             for itinerary in itineraries[keyword]:
-        
+       
+                # POST itinerary
+                postItinerary(itinerary)
+ 
+                # GET ports
                 ports = getPorts(itinerary["id_itinerario"])
 
-                print 'Getting Initial Manifest Associated to each itinerary and port'
+                #print 'Getting Initial Manifest Associated to each itinerary and port'
                 for port_id in ports:
                     for p in ports[port_id]:
+
+                        # POST Port
+
+                        # POST Manifest
                         manifest = getInitialManifest(itinerary["id_itinerario"], p['id_ubicacion'])
 
     elif opt in ("-u", "--update"):
@@ -85,12 +111,12 @@ for opt, arg in opts:
 
         itineraries = getItineraries(date)
 
-        print 'Getting Ports Associated to each itinerary'
+        #print 'Getting Ports Associated to each itinerary'
         for keyword in itineraries:
             for itinerary in itineraries[keyword]:
                 ports = getPorts(itinerary["id_itinerario"])
         
-                print 'Getting Initial Manifest Associated to each itinerary and port'
+                #print 'Getting Initial Manifest Associated to each itinerary and port'
                 for port_id in ports:
                     for p in ports[port_id]:
                         manifest = getUpdatedManifest(itinerary["id_itinerario"], p['id_ubicacion'], update_time)
