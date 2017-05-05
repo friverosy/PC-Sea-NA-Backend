569e3f21762550abc9e15309abdb7ff048e86233
Tzu-Chiang Shen <tshen@blueshadows.cl>
2017-05-05 02:49:55 +0000

    NAV-86: change 'name' to be no mandatory field.

--- server/api/person/person.model.js
+++ server/api/person/person.model.js
@@ -3,7 +3,7 @@
 import mongoose from 'mongoose';
 
 var PersonSchema = new mongoose.Schema({
-  name: { type: String, uppercase: true, required: true },
+  name: { type: String, uppercase: true, required: false },
   sex: String,
   resident: String,
   nationality: String,
